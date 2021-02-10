d2959a5040c508f14527c95c6c0d6ecb5d1f5913
zoe-hepey <niezyeoh1999@gmail.com>
2021-02-11 01:00:18 +0800

    Site updated: 2021-02-11 01:00:11

--- js/duration-real.js
+++ js/duration-real.js
@@ -1,7 +1,7 @@
 !(function() {
     function update() {
       var now = new Date();
-      var grt = new Date("2020-08-11 11:18:00");  /** 此处是计时的起始时间 **/
+      var grt = new Date("2020-08-11 23:18:00");  /** 此处是计时的起始时间 **/
       now.setTime(now.getTime()+250);
       days = (now - grt ) / 1000 / 60 / 60 / 24;
       dnum = Math.floor(days);
